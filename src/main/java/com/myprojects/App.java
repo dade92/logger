0a97cf48bac9c84ecd6c7c6bc5e32dc31cb19f75
davide <davidebotti92@gmail.com>
2021-05-07 12:29:21 +0200

    First version of the library

---
+++ src/main/java/com/myprojects/App.java
@@ -0,0 +1,13 @@
+package com.myprojects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
